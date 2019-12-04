98e62de632f0bec3266f2dc40002afad84ae82eb
Ricardo Cuevas Escalante <ricardocuevasescalante@Ricardos-MacBook-Pro.local>
2019-12-04 12:52:55 +0700

    chnage some migration versions

--- migrations/versions/1c5a499ec99f_.py
+++ migrations/versions/1c5a499ec99f_.py
@@ -8,7 +8,6 @@ Create Date: 2019-12-02 13:04:54.615991
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
 revision = '1c5a499ec99f'
 down_revision = '3e3f7a8a37ce'
