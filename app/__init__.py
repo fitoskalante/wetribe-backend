1fca700dc3e8b1fcd7b0e5fe762585ab4d9ce18c
Ricardo Cuevas Escalante <ricardocuevasescalante@Ricardos-MacBook-Pro.local>
2019-12-04 12:28:15 +0700

    initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
-from flask import Flask, redirect, url_for, flash, render_template
-from flask_login import login_required, logout_user
+from flask import Flask, redirect, url_for, flash, render_template, jsonify, request
+from flask_login import login_required, logout_user, current_user
 from .config import Config
-from .models import db, login_manager
+from .models import db, login_manager, User, Token
 from .oauth import blueprint
 from .cli import create_db
-
+from flask_migrate import Migrate
+from flask_cors import CORS
+import uuid
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,15 +15,80 @@ app.cli.add_command(create_db)
 db.init_app(app)
 login_manager.init_app(app)
 
+migrate = Migrate(app, db)
+CORS(app)
 
-@app.route("/logout")
+
+@app.route("/logout", methods=['GET'])
 @login_required
 def logout():
+    token = Token.query.filter_by(user_id=current_user.id).first()
+    if token:
+        db.session.delete(token)
+        db.session.commit()
     logout_user()
-    flash("You have logged out")
-    return redirect(url_for("index"))
+    res = {'success': True, 'message': 'success'}
+    return jsonify(res)
 
 
 @app.route("/")
 def index():
     return render_template("home.html")
+
+
+@app.route('/getuser')
+@login_required
+def getuser():
+    return jsonify({
+        'id': current_user.id,
+        'name': current_user.name,
+        'email': current_user.email,
+        'token': current_user.token[0].uuid
+    })
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    if request.method == 'POST':
+        data = request.get_json()
+        user = User.query.filter_by(email=data['email']).first()
+        if not user:
+            user = User(
+                name=data['name'],
+                email=data['email'],
+            )
+            user.set_password(data['password'])
+            user.add()
+            res = {'success': True, "message": "Success"}
+            return jsonify(res)
+        res = {'success': False, "message": "This email is already registered"}
+        return jsonify(res)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        data = request.get_json()
+        user = User.query.filter_by(email=data['email']).first()
+        if not user:
+            res = {"success": False, "message": "This email is not registered"}
+            return jsonify(res)
+        if not user.check_password(data['password']):
+            res = {
+                "success": False,
+                "message": "Wrong Password, please try again"
+            }
+            return jsonify(res)
+        token = Token.query.filter_by(user_id=user.id).first()
+        if not token:
+            token = Token(uuid=str(uuid.uuid4().hex), user_id=user.id)
+            token.add()
+        res = {
+            "success": True,
+            "user": {
+                "name": user.name,
+                'email': user.email
+            },
+            "token": token.uuid
+        }
+        return jsonify(res)
--- app/models.py
+++ app/models.py
@@ -1,14 +1,30 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin
 from flask_dance.consumer.storage.sqla import OAuthConsumerMixin
-
+from werkzeug.security import generate_password_hash, check_password_hash
+import uuid
 
 db = SQLAlchemy()
 
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(256), unique=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String)
+    password = db.Column(db.String)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def check_user(self):
+        return User.query.filter_by(email=self.email).first()
+
+    def add(self):
+        db.session.add(self)
+        db.session.commit()
 
 
 class OAuth(OAuthConsumerMixin, db.Model):
@@ -17,11 +33,35 @@ class OAuth(OAuthConsumerMixin, db.Model):
     user = db.relationship(User)
 
 
+class Token(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uuid = db.Column(db.String, unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
+    user = db.relationship(User, backref='token', lazy=True)
+
+    def add(self):
+        db.session.add(self)
+        db.session.commit()
+
+
 # setup login manager
 login_manager = LoginManager()
 login_manager.login_view = "facebook.login"
+
+login_manager = LoginManager()
 
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+
+@login_manager.request_loader
+def load_user_from_request(request):
+    api_key = request.headers.get('Authorization')
+    if api_key:
+        api_key = api_key.replace('Token ', '', 1)
+        token = Token.query.filter_by(uuid=api_key).first()
+        if token:
+            return token.user
+    return None
